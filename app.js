44c3459ec15d0fcbc4e14895e2ebf219128b7200
Akvile4 <aqua0818@gmail.com>
2022-01-06 19:42:21 +0200

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const key = document.querySelector('.key-result');
+const keyPress = document.getElementById('keycode-display');
+const key = document.getElementById('key');
+const keyCode = document.getElementById('code');
+const keyWhich = document.getElementById('which')
 
-// key.addEventListener('keydown', ())
+document.addEventListener('keydown', (event) => {
+    key.textContent = event.key;
+    keyCode.textContent = event.code;
+    keyWhich.textContent = event.which;
+    keyPress.textContent = event.which;
+});
